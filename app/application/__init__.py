d5e1dd75f2219f1a4532ed6d55f0c83b1207d074
Mia <mia_bajic@live.com>
2022-03-25 14:37:20 +0100

    added models

--- app/application/__init__.py
+++ app/application/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 import os
 from .views import page_not_found
 # from config import *
+from flask import Flask
+from flask_migrate import Migrate
 
 
 def create_app():
@@ -10,13 +12,13 @@ def create_app():
     https://flask.palletsprojects.com/en/2.0.x/patterns/appfactories/
     """
     application = Flask(__name__, template_folder="templates")
-    # application.config.from_object(os.environ["APP_SETTINGS"])
+    application.config.from_object(os.environ["APP_SETTINGS"])
     application.register_error_handler(404, page_not_found)
 
     from .models import db
-    #
-    # db.init_app(application)
-    # migrate = Migrate(application, db)
+
+    db.init_app(application)
+    migrate = Migrate(application, db)
 
     from .views import app
 
--- app/application/models.py
+++ app/application/models.py
@@ -1,3 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Rule(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    support = db.Column(db.Float, nullable=False)
+    confidence = db.Column(db.Float, nullable=False)
+    lift = db.Column(db.Float, nullable=False)
+
+
+class Measure(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    value = db.Column(db.String(100), nullable=False)
+
+
+class RuleMeasure(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rule_id = db.Column(db.Integer, db.ForeignKey(Rule.id), nullable=False)
+    measure_id = db.Column(db.Integer, db.ForeignKey(Measure.id), nullable=False)
