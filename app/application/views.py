0c7a07addf783c7b71d4c2ff1008d5d87d474cd8
Mia <mia_bajic@live.com>
2022-04-14 01:05:40 +0200

    added index page

--- app/application/views.py
+++ app/application/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 import logging
+from .models import db, Measure
 
 logging.basicConfig()
 logger = logging.getLogger("sqlalchemy.engine")
@@ -12,3 +13,10 @@ app = Blueprint("app", __name__)
 def page_not_found(e):
     """Returns error template, if user enters wrong URL."""
     return render_template("404.html"), 404
+
+
+@app.route("/", methods=["GET", "POST"])
+def get_measures():
+    """Gets list of all measures"""
+    if request.method == "GET":
+        return render_template("index.html")
