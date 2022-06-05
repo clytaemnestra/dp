6af39a074aae71336f102668ea78fc99eb05a4d1
Mia <mia_bajic@live.com>
2022-06-05 14:32:24 +0200

    refactoring the search functions

---
+++ app/application/search.py
@@ -0,0 +1,82 @@
+from .models import db, Measure, Rule, RuleMeasure
+from sqlalchemy import select
+
+
+def exact_search(selected_values):
+    """Returnes rules, where """
+    statement = select(Rule.id)
+    for key, value in selected_values.items():
+        statement = statement.where(
+            select(RuleMeasure.id)
+            .join(Measure)
+            .where(RuleMeasure.rule_id == Rule.id)
+            .where(Measure.name == key)
+            .where(Measure.description == value)
+            .exists()
+        )
+
+    rule_id = db.session.execute(statement).all()
+    return rule_id
+
+
+def loose_search(measure_name, measure_description):
+    rule_id = (
+        db.session.query(RuleMeasure.rule_id)
+        .join(Measure, RuleMeasure.measure_id == Measure.id)
+        .filter(
+            Measure.name == measure_name, Measure.description == measure_description
+        )
+        .all()
+    )
+    return rule_id
+
+
+def transform_query_data_to_dict(rule_id):
+    """Every rule returned by functions 'loose_search' and 'exact_search' is in the form of a tuple. It's necessary
+    to transform it to dictionary, so it could be used by front-end."""
+    # converts tuples to list
+    rules_list = [v[0] for v in rule_id]
+    rules_dict = {}
+    metrics_list = []
+
+    # finds support, confidence & lift for given rules
+    for r in rules_list:
+        metrics = (
+            db.session.query(Rule.support, Rule.confidence, Rule.lift)
+            .join(RuleMeasure, Measure.id == RuleMeasure.measure_id)
+            .join(Measure, RuleMeasure.measure_id == Measure.id)
+            .filter(Rule.id == r)
+            .distinct()
+            .all()
+        )
+        metrics_list.append(metrics)
+
+        # adds metrics as value for rule id
+        rules_dict[r] = []
+        for i in metrics:
+            rules_dict[r].append(i)
+
+        # finds all other measures which are connected with the given rule id
+        measures = (
+            db.session.query(Measure.name, Measure.description)
+            .join(RuleMeasure, Measure.id == RuleMeasure.measure_id)
+            .filter(RuleMeasure.rule_id == r)
+            .all()
+        )
+        # adds measures to the final dict
+        for j in measures:
+            rules_dict[r].append(j)
+    return rules_dict
+
+
+"""
+1. funkce - exact search 
+jako vstup bere selected_values
+jako vystup rule_id
+2. funkce - loose search
+jako vstup bere selected_values
+jako vystup rule_id
+3. funkce query to dict
+vstup rule_id
+vystup rules_dict 
+"""
--- app/application/views.py
+++ app/application/views.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, render_template, request, redirect
 import logging
-from .models import db, Measure, Rule, RuleMeasure
-import ast
-from sqlalchemy import select, and_
+from .models import db, Measure
+from .search import exact_search, loose_search, transform_query_data_to_dict
 
 logging.basicConfig()
 logger = logging.getLogger("sqlalchemy.engine")
@@ -19,7 +18,14 @@ def page_not_found(e):
 
 @app.route("/", methods=["GET", "POST"])
 def get_measures():
-    """Gets list of all measures"""
+    """
+    Gets list of all measures. The search algorithm works in the following way:
+    1.
+    2.
+    3.
+    4.
+    5.
+     """
     measures_dict = {}
     if request.method == "GET":
         measures = db.session.query(Measure.name, Measure.description).all()
@@ -43,14 +49,7 @@ def get_measures():
 
 @app.route("/results", methods=["GET", "POST"])
 def get_results():
-    """
-    The main algorithm, which filters through rules and measures. It works in the following way:
-    1. takes all selected measures & values from form on the FE,
-    2. finds corresponding rules which contain all selected measures & values,
-    3. for all rules returns confidence, support & lift,
-    4. for all rules finds all other corresponding measures & values.
-    """
-    rule_ids = []
+    """Gets list of rules for given measures."""
     f = request.form
     selected_values = {}
     for key in f.keys():
@@ -63,57 +62,9 @@ def get_results():
     for key, value in selected_values.items():
         measure_name = key
         measure_description = value
-    # finds all corresponding rules
-    rule_id = (
-        db.session.query(RuleMeasure.rule_id)
-        .join(Measure, RuleMeasure.measure_id == Measure.id)
-        .filter(
-            Measure.name == measure_name, Measure.description == measure_description
-        )
-        .all()
-    )
 
-    # bugfix:
-    # statement = select(Rule)
-    # for key, value in selected_values.items():
-    #     statement = statement.where(
-    #         select(RuleMeasure.id).join(Measure)
-    #             .where(RuleMeasure.rule_id == Rule.id)
-    #             .where(Measure.name == key)
-    #             .where(Measure.value == value).exists())
+    # rule_id = exact_search(selected_values)
+    rule_id = loose_search(measure_name, measure_description)
+    rules_dict = transform_query_data_to_dict(rule_id)
 
-    # converts tuples to list
-    rules_list = [v[0] for v in rule_id]
-    rules_dict = {}
-    metrics_list = []
-
-    # finds support, confidence & lift for given rules
-    for r in rules_list:
-        metrics = (
-            db.session.query(Rule.support, Rule.confidence, Rule.lift)
-            .join(RuleMeasure, Measure.id == RuleMeasure.measure_id)
-            .join(Measure, RuleMeasure.measure_id == Measure.id)
-            .filter(Rule.id == r)
-            .distinct()
-            .all()
-        )
-        metrics_list.append(metrics)
-
-        # adds metrics as value for rule id
-        rules_dict[r] = []
-        for i in metrics:
-            rules_dict[r].append(i)
-
-        # finds all other measures which are connected with the given rule id
-        measures = (
-            db.session.query(Measure.name, Measure.description)
-            .join(RuleMeasure, Measure.id == RuleMeasure.measure_id)
-            .filter(RuleMeasure.rule_id == r)
-            .all()
-        )
-        # adds measures to the final dict
-        for j in measures:
-            rules_dict[r].append(j)
-    print(rules_dict)
     return render_template("results.html", rules_dict=rules_dict)
-
